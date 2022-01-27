43e8f676f8751f57b2dee2854ea0d4d0a7ab99c2
Samaritan <cihad3194@gmail.com>
2022-01-26 23:16:45 -0500

    commit 10

--- src/main/java/TestCase001.java
+++ src/main/java/TestCase001.java
@@ -10,6 +10,6 @@ public class TestCase001 {
         System.out.println("bu defer olsun");
         System.out.println("ahanda yeni bise ekledim enes abi");
         System.out.println("yeter bit artik");
-        System.out.println("dahda gasik");
+        System.out.println("dahda gasik ");
     }
 }
