8d97668cee7835354896b84fc49b0e9c9503532d
Samaritan <cihad3194@gmail.com>
2022-01-26 23:06:57 -0500

    commit 7

--- src/main/java/TestCase001.java
+++ src/main/java/TestCase001.java
@@ -1,7 +1,7 @@
 public class TestCase001 {
     public static void main(String[] args) { // main method created
         System.out.println("Hello Zonguldak"); // hello Zonguldak written
-// cihad started committing
+        // cihad started committing
         // this one is the second step
         // this one is the third
         //ljasbfsa
