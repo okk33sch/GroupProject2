52e5750e05f2fb7fb02c2f383d13aa79357dc0bb
enes <enesuzun2020@gmail.com>
2022-01-26 22:13:19 -0500

    second comment

--- src/main/java/TestCase001.java
+++ src/main/java/TestCase001.java
@@ -2,5 +2,6 @@ public class TestCase001 {
     public static void main(String[] args) { // main method created
         System.out.println("Hello Zonguldak"); // hello Zonguldak written
         System.out.println("Hello to CanAda");
+        System.out.println("Enes was here ");
     }
 }
